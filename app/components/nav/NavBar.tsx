a18f1d5de4d5fdc666452bf90ef32c4d8688049f
RafaelCaetanoSilvaPcal <raphaelcamoes26@gmail.com>
2024-01-16 17:59:32 -0300

    first commit

---
+++ app/components/nav/NavBar.tsx
@@ -0,0 +1,28 @@
+import  Container  from "../Container";
+
+const NavBar = ()=> {
+   return <div className="
+   sticky
+   top-0
+   w-full
+   bg-slate-200
+   z-30
+   shadow-sm
+
+   ">
+        <div className="py-4 border-b-[1px]">
+            <Container>
+                <div>
+                    <link href="/">E-Shop</link>
+                    <div>Search</div>
+                    <div>
+                        <div>CartCount</div>
+                        <div>UserMenu</div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+   </div>
+};
+
+export default NavBar;
