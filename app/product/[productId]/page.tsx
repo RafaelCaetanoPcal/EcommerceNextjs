d9b7ad80470bf6fb7980f384526514160a1a5846
RafaelCaetanoSilvaPcal <raphaelcamoes26@gmail.com>
2024-01-22 17:35:18 -0300

    changes in E-shop

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,11 +1,23 @@
+import Container from "@/app/components/Container";
+import { product } from "@/app/utils/product";
+import ProductDetails from "./ProductDetails";
+
+
+
 interface IPrams {
     productId?: string;
 }
 
 const Product  = ({params} : { params: IPrams}) => {
    console.log('params', params )
-   return <div>Product
-    Page
-    </div>;
+  
+    product
+  return (
+    <div className="p-8">
+        <Container>
+            <ProductDetails product = {product} />
+        </Container>
+        </div>
+  );
 };
 export default Product;
